ec4a83d1a9c9346904f7f5252d660835ae4fa6b8
Mina Nabil <mina9492@hotmail.com>
2020-06-23 00:50:37 +0200

    merged

---
+++ ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/Users/mina/Documents/FrontEnd/flutter/src/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/mina/Documents/FrontEnd/flutter/motorcity"
+export "FLUTTER_TARGET=lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "SYMROOT=${SOURCE_ROOT}/../build/ios"
+export "OTHER_LDFLAGS=$(inherited) -framework Flutter"
+export "FLUTTER_FRAMEWORK_DIR=/Users/mina/Documents/FrontEnd/flutter/src/flutter/bin/cache/artifacts/engine/ios-release"
+export "FLUTTER_BUILD_NAME=1.6.0"
+export "FLUTTER_BUILD_NUMBER=11"
